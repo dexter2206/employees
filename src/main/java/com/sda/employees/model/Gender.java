41b890d9cbe5ec3d83b1aa00f02682a04fc600d7
Konrad Jałowiecki <dexter2206@gmail.com>
2018-02-10 05:58:22 +0100

    Add method to construct Gender Instance from repository strings

--- src/main/java/com/sda/employees/model/Gender.java
+++ src/main/java/com/sda/employees/model/Gender.java
@@ -1,5 +1,15 @@
 package com.sda.employees.model;
 
 public enum Gender {
-    MALE, FEMALE
+    MALE, FEMALE;
+
+    public static Gender fromInitial(String initial) {
+        if(initial.equalsIgnoreCase("M")) {
+            return Gender.MALE;
+        }
+        if(initial.equalsIgnoreCase("F")) {
+            return Gender.FEMALE;
+        }
+        throw new RuntimeException("Cannot construct member of Gender enum from " + initial);
+    }
 }
