472056cff39680c7e08755066b5e8d55a661e621
Konrad Jałowiecki <dexter2206@gmail.com>
2018-07-17 20:21:47 +0200

    Add create and delete method to DepartmentSqlDAO

--- src/main/java/com/sda/employees/dao/DepartmentSqlDAO.java
+++ src/main/java/com/sda/employees/dao/DepartmentSqlDAO.java
@@ -13,12 +13,18 @@ public class DepartmentSqlDAO implements DepartmentDAO{
     private DataSource ds;
     final String byDeptNoQuery = "SELECT * FROM departments WHERE dept_no = ?";
     final String createStatement = "INSERT INTO departments(dept_no, dept_name) VALUES(?, ?)";
+    final String updateStatement = "UPDATE departments SET dept_name = ? WHERE dept_no = ?";
+    final String deleteStatement = "DELETE FROM departments WHERE dept_no = ?";
+
     public DepartmentSqlDAO(DataSource ds) {
         this.ds = ds;
     }
 
     @Override
     public Department getByNo(String deptNo) {
+        if(deptNo == null) {
+            throw new IllegalArgumentException("Invalid department number");
+        }
         ResultSet rs = null;
         try(Connection conn = ds.getConnection();
             PreparedStatement stmt = conn.prepareStatement(byDeptNoQuery)) {
@@ -56,7 +62,17 @@ public class DepartmentSqlDAO implements DepartmentDAO{
 
     @Override
     public void update(Department d) {
-
+        try(Connection conn = ds.getConnection();
+        PreparedStatement ps = conn.prepareStatement(updateStatement)) {
+            ps.setString(1, d.getDeptName());
+            ps.setString(2, d.getDeptNo());
+            if(ps.executeUpdate() != 1) {
+                throw new RuntimeException("Something odd happened.");
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Updating department failed");
+        }
     }
 
     @Override
